5eb7f2321e430aaab44d73992a69b76d860f2207
George991808 <gellis7@gmail.com>
2022-02-13 17:50:44 +0800

    creates a readme file with the first question answered

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+
 const questions = 
     {
         type: "input",
         name: "repoName",
-        message: "What is the name of you Repo?"
+        message: "What is the name of your Repo?"
     };
 
 function askQuestions() {
     inquirer.prompt(questions).then((responses) => {
         console.log(responses);
       
-        
+        fs.writeFile('./Readme output/README.md', generateText(responses), (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
     })
 }
 askQuestions();
+
+function generateText(data) {
+    return `
+    # ${data.repoName}
+    
+    
+  `;
+  }
+  
