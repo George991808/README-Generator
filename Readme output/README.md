bd2d55c40f6b88b9cdf44651507e62a8663a7f93
George991808 <gellis7@gmail.com>
2022-02-13 18:11:41 +0800

    added the required questions

--- Readme output/README.md
+++ Readme output/README.md
@@ -1,5 +1,5 @@
 
-    # sdfsd
+    # xcfvcx
     
     
   
--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const questions = 
+const questions = [
     {
         type: "input",
         name: "repoName",
         message: "What is the name of your Repo?"
-    };
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your Repo?"
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
 
+        type: "list",
+        name: "license",
+        message: "What is your license?",
+        choices: ["Apache", "GNU", "MIT", "No License"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Write installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is repo used for?"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Who contributed to the project?"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "List testing procedure"
+    }
+
+]
 function askQuestions() {
     inquirer.prompt(questions).then((responses) => {
         console.log(responses);
